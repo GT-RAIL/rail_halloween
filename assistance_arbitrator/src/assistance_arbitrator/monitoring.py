426ec481c4d5c919312a6c046b5f408b4808b6fe
Siddhartha Banerjee <banerjs.sid@gmail.com>
2018-10-25 10:53:56 -0400

    Created a new launch file and updated the task actions based on what we expect

--- assistance_arbitrator/src/assistance_arbitrator/monitoring.py
+++ assistance_arbitrator/src/assistance_arbitrator/monitoring.py
@@ -184,20 +184,20 @@ class ROSGraphMonitor(object):
         "/database/arm_joint_pose",
         "/database/trajectory",
         "/execute_kinematic_path",
-        # find_grasps
-        "/suggester/suggest_grasps",
-        "/suggester/pairwise_rank",
-        # find_object
-        "/database/object_constraints",
-        "/rail_segmentation/segment_objects",
-        "/grasp_executor/add_object",
-        "/grasp_executor/clear_objects",
+        # # find_grasps
+        # "/suggester/suggest_grasps",
+        # "/suggester/pairwise_rank",
+        # # find_object
+        # "/database/object_constraints",
+        # "/rail_segmentation/segment_objects",
+        # "/grasp_executor/add_object",
+        # "/grasp_executor/clear_objects",
         # listen
         "/get_last_speech_cmd",
-        # move
-        "/database/waypoints",
-        # place
-        "/grasp_executor/drop_object",
+        # # move
+        # "/database/waypoints",
+        # # place
+        # "/grasp_executor/drop_object",
         # toggle_breakers
         "/arm_breaker",
         "/base_breaker",
