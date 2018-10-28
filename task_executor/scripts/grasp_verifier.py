533faad1320818c7c509cba4a44d39fbc1bfef91
Siddhartha Banerjee <banerjs.sid@gmail.com>
2018-10-27 21:23:57 -0400

    Small updates to the verifier

--- task_executor/scripts/grasp_verifier.py
+++ task_executor/scripts/grasp_verifier.py
@@ -14,19 +14,16 @@ class GraspVerifier(object):
     """
 
     VERIFY_SERVICE = '~verify'
-    STOP_SERVICE = '~stop'
-    SETUP_SERVICE = '~setup'
-    TRIGGER_SERVICE = '~trigger'
 
     def __init__(self):
-        # The background spinner
-        self._background_spinner = None
-        self._spin = False
-        self._trigger = False  # TODO: Use a wake-word or something smarter
         self.bridge = CvBridge()
         self.image_sub_topic_name = rospy.get_param('~image_sub_topic_name', default='/head_camera/depth/image_rect')
         self.last_image = np.zeros((640, 480))
         self.image_lock = Lock()
+
+        # Create the subscriber
+        self._depth_sub = rospy.Subscriber(self.image_sub_topic_name, Image, self._parse_image)
+
         # Create the services
         self._verify_service = rospy.Service(GraspVerifier.VERIFY_SERVICE, Trigger, self._verify_grasp)
 
@@ -39,18 +36,21 @@ class GraspVerifier(object):
                 success = False
             else:
                 success = True
+
         return TriggerResponse(success=success)
 
     def _parse_image(self, image_msg):
         try:
             image_cv = self.bridge.imgmsg_to_cv2(image_msg, "uint16")
         except CvBridgeError as e:
-            print e
-            return -1
-        self.last_image = image_cv
+            rospy.logerr("{}".format(e))
+            return
+
+        if self.image_lock.acquire(False):
+            self.last_image = image_cv
+            self.image_lock.release()
 
     def run(self):
-        rospy.Subscriber(self.image_sub_topic_name, Image, self._parse_image)
         rospy.spin()
 
 
