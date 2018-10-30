5fca709aac6d0cbdf56b32e6d06623f4f4b1f958
Siddhartha Banerjee <banerjs.sid@gmail.com>
2018-10-30 14:02:40 -0400

    Add gripper to the grasp verification

--- candy_manipulation/scripts/grasp_verifier.py
+++ candy_manipulation/scripts/grasp_verifier.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # Created by Andrew Silva on 10/26/18
-from sensor_msgs.msg import Image
-from std_srvs.srv import Trigger, TriggerResponse
-from threading import Lock
-import rospy
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
 import numpy as np
+
+import rospy
+import cv2
+
+from cv_bridge import CvBridge, CvBridgeError
+
+from sensor_msgs.msg import Image
+from fetch_driver_msgs.msg import GripperState
+from std_srvs.srv import Trigger, TriggerResponse
 
 
 class GraspVerifier(object):
@@ -15,7 +18,10 @@ class GraspVerifier(object):
     """
 
     VERIFY_SERVICE = '~verify'
+
     IMAGE_TOPIC = '/head_camera/depth/image_rect'
+    GRIPPER_STATE_TOPIC = '/gripper_state'
+
     DEFAULT_SUCCEED_SERVICE = '~default_success'
     DEFAULT_FAIL_SERVICE = '~default_fail'
     SIMULATION_PARAMETER = '/use_sim_time'
@@ -23,19 +29,17 @@ class GraspVerifier(object):
     def __init__(self):
         self.bridge = CvBridge()
         self.last_image = np.zeros((640, 480))
-        self.image_lock = Lock()
 
         # We don't actually want to verify in sim
         self._in_simulation = rospy.get_param(GraspVerifier.SIMULATION_PARAMETER, False)
 
-        # Create the subscriber
-        self._depth_sub = rospy.Subscriber(GraspVerifier.IMAGE_TOPIC, Image, self._parse_image)
-
-        # Create the services
-        self._verify_service = rospy.Service(GraspVerifier.VERIFY_SERVICE, Trigger, self._verify_grasp)
-
-        # The variable to threshold detection on
+        # The variables to threshold detection on
         self.img_sum = 0.0
+        self.gripper_pos = 0.05
+
+        # Create the subscribers
+        self._depth_sub = rospy.Subscriber(GraspVerifier.IMAGE_TOPIC, Image, self._parse_image)
+        self._gripper_sub = rospy.Subscriber(GraspVerifier.GRIPPER_STATE_TOPIC, GripperState, self._parse_gripper_state)
 
         # Should we succeed or fail by default?
         self._default_succeed = False
@@ -50,6 +54,9 @@ class GraspVerifier(object):
             self._set_default_fail
         )
 
+        # Create the services
+        self._verify_service = rospy.Service(GraspVerifier.VERIFY_SERVICE, Trigger, self._verify_grasp)
+
     def _set_default_succeed(self, req):
         self._default_succeed = True
         return TriggerResponse(success=True)
@@ -60,33 +67,31 @@ class GraspVerifier(object):
 
     def _verify_grasp(self, req):
         success = self._default_succeed
-        # FIXME: Fix the parameters here
-        # with self.image_lock:
-        #     image_in = np.array(self.last_image[145:270, 365:465])
-        #     image_in[image_in > 20] = 0
         msg = str(self.img_sum)
         if not self._in_simulation:
-            if self.img_sum < 11000:
+            if self.img_sum < 11000 or self.gripper_pos >= 0.005:
                 success = True
             else:
                 success = False
 
         return TriggerResponse(success=success, message=msg)
 
+    def _gripper_sub(self, gripper_msg):
+        try:
+            self.gripper_pos = gripper_msg.joints[0].position
+        except Exception as e:
+            rospy.logerr("Grasp Verifier: {}".format(e))
+
     def _parse_image(self, image_msg):
         try:
             image_cv = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding="passthrough")  # The encoding is 16UC1
         except CvBridgeError as e:
-            rospy.logerr("{}".format(e))
+            rospy.logerr("Grasp Verifier: {}".format(e))
             return
         image_cv = np.array(image_cv[145:270, 365:465])
         image_cv[image_cv > 20] = 0
         image_cv[np.isnan(image_cv)] = 0
         self.img_sum = np.sum(image_cv)
-
-        # if self.image_lock.acquire(False):
-        #     self.last_image = image_cv
-        #     self.image_lock.release()
 
     def run(self):
         rospy.spin()
